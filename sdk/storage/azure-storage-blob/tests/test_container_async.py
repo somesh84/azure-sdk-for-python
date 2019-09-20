d0a15ea23388c574f10a8485835e29f29ad58c4b
Laurent Mazuel <laurent.mazuel@gmail.com>
2019-09-20 10:58:30 -0700

    Storage clean-up

--- sdk/storage/azure-storage-blob/tests/test_container_async.py
+++ sdk/storage/azure-storage-blob/tests/test_container_async.py
@@ -1212,7 +1212,7 @@ class StorageContainerTestAsync(StorageTestCase):
 
         # Act
         response = await container.delete_blobs(
-            'blob12',
+            'blob1',
             'blob2',
             'blob3',
         )
